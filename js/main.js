19237003e4a08b349c7b612dc3ed54a15dd8993e
Linh Nguyen <linhgbale0410@gmail.com>
2022-01-16 18:10:16 +0700

    finance page

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $(".section-employer").slick({
+    slidesToShow: 3,
+    infinite: true,
+    speed: 800,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
+$(document).ready(function () {
+  $(".section-news_list").slick({
+    slidesToShow: 3,
+    infinite: true,
+    speed: 800,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
